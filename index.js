e7d68d7409c9365b70c953df14b3b5b96e5214dc
Beast <53580315+Kappaladi@users.noreply.github.com>
2021-11-03 20:20:47 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const chalk = require('chalk');
+const RPC = require('discord-rpc')
+const browser = typeof window !== 'undefined';
+const rpc = new RPC.Client({ transport: browser ? 'websocket' : 'ipc' });
+const config = require('./config/config.json');
+
+
+
+const title = `
+______   _______  _______  _______ _________
+(  ___ \ (  ____ \(  ___  )(  ____ \\__   __/
+| (   ) )| (    \/| (   ) || (    \/   ) (   
+| (__/ / | (__    | (___) || (_____    | |   
+|  __ (  |  __)   |  ___  |(_____  )   | |   
+| (  \ \ | (      | (   ) |      ) |   | |   
+| )___) )| (____/\| )   ( |/\____) |   | |   
+|/ \___/ (_______/|/     \|\_______)   )_(   
+   Discord Rich Presence made easy by Beast                   
+                ||DISCLAIMER||      
+My version is safe and wont get you banned like others. 
+                   Enjoy.   
+
+`
+
+
+
+console.log(chalk.greenBright(title))
+
+rpc.on("ready", () => {
+    rpc.setActivity({
+        details: `${config.details}`,
+        state: `${config.state}`,
+        startTimestamp: new Date(),
+        largeImageKey: `${config.largeImageKey}`,
+        largeImageText: `${config.largeImageText}`,
+        smallImageKey: `${config.smallImageKey}`,
+        smallImageText: `${config.smallImageText}`
+})})
+
+
+rpc.login({
+    clientId: `${config.cid}`,
+})
+
+
+
+/*
+rpc.on('ready', () => {
+    console.log(chalk.greenBright(title))
+    rpc.setActivity({
+        details: config.details,
+        state: config.state,
+        startTimestamp: new Date(),
+        largeImageKey: config.largeImageKey,
+        largeImageText: config.largeImageText,
+        smallImageKey: config.smallImageKey,
+        smallImageText: config.smallImageText
+    })
+}) */
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+title Beast Rich Presence
+cls
+node index.js
